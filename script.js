9819b2387cd8471174222e0004d6892d2dc4e0f8
MariaKara1 <144834772+MariaKara1@users.noreply.github.com>
2023-10-10 10:37:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+Let augustExpenses = [
+{expense: “groceries”, category: “necessities”, cost: 300}
+{expense: “utilities”, category: “necessities”, cost: 150}
+{expense: “car repairs”, category: “necessities”, cost: 100}
+{expense: “dining out”, category: “luxury”, cost: 150 }
+{expense: “entertainment”, category: “luxury”, cost: 200 }
+{expense: “birthday gift”, category: “gift”, cost: 100}
+]
